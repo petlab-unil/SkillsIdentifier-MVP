d581b07cece1f770a058f77aab625e39fa685443
sakex <alexandre@senges.ch>
2020-08-01 13:59:35 +0200

    Deployment:
    - Change dep mysql => mysql2
    - New mysql.Dockerfile
    - New Init.sql

--- server/server.js
+++ server/server.js
@@ -1,111 +1,104 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const logger = require("morgan");
-const methodOverride = require("method-override");
-const cors = require("cors");
-const util = require('util');
+(async () => {
+
+  const express = require("express");
+  const bodyParser = require("body-parser");
+  const logger = require("morgan");
+  const methodOverride = require("method-override");
+  const cors = require("cors");
 
 
-const app = express();
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(methodOverride());
-app.use(cors());
+  const app = express();
+  app.use(logger("dev"));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cors());
 
-const {MYSQL_ROOT_PASSWORD, DB_HOSTNAME} = process.env;
+  const {MYSQL_ROOT_PASSWORD, DB_HOSTNAME} = process.env;
 
-const mysql = require("mysql");
-const connection = mysql.createConnection({
-  host: DB_HOSTNAME,
-  user: "root",
-  password: MYSQL_ROOT_PASSWORD,
-  database: "SkillsIdentifier",
-});
-
-
-const del = connection._protocol._delegateError;
-connection._protocol._delegateError = function(err, sequence){
-  if (err.fatal) {
-    console.trace('fatal error: ' + err.message);
-  }
-  return del.call(this, err, sequence);
-};
-
-const asyncQuery = util.promisify(connection.query).bind(connection);
-
+  const mysql = require("mysql2/promise");
+  const connection = await mysql.createConnection({
+    host: DB_HOSTNAME,
+    user: "root",
+    password: MYSQL_ROOT_PASSWORD,
+    database: "SkillsIdentifier",
+    //charset: "utf8mb4",
+    charset: "UTF8MB4",
+    port:3306
+  });
 
 // test api
-app.get("/test", async (req, res) => {
-  const query = "select * from ParentJobTranslation";
-  try {
-    const rows = await asyncQuery(query);
-    console.log(rows);
-    res.json(rows);
-  } catch (e) {
-    res.status(500);
-    res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
-  }
-});
+  app.get("/test", async (req, res) => {
+    const query = "select * from ParentJobTranslation";
+    try {
+      const [rows, _] = await connection.query(query);
+      res.json(rows);
+    } catch (e) {
+      res.status(500);
+      res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
+    }
+  });
 
 
 // onet soc code to French api
-app.get("/parentjobfrench/:onetsoccode", async (req, res) => {
-  const query = `select * from ParentJobTranslation where ONETSOCCode = "${req.params.onetsoccode}"`;
-  try {
-    const rows = await asyncQuery(query);
-    if (rows.length)
-      res.json(rows[0].French);
-    else res.json("");
-  } catch (e) {
-    res.status(500);
-    res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
-  }
-});
+  app.get("/parentjobfrench/:onetsoccode", async (req, res) => {
+    const query = `select * from ParentJobTranslation where ONETSOCCode = "${req.params.onetsoccode}"`;
+    try {
+      const [rows, _] = await connection.query(query);
+      if (rows.length)
+        res.json(rows[0].French);
+      else res.json("");
+    } catch (e) {
+      res.status(500);
+      res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
+    }
+  });
 
 // skill uuid to French
-app.get("/skillfrench/:uuid", async (req, res) => {
-  const query = `select * from SkillTranslation where SkillUUId = '${req.params.uuid}'`;
-  try {
-    const rows = await asyncQuery(query);
-    if (rows.length > 0) {
-      res.json(rows[0].French);
-    } else {
-      res.json("");
+  app.get("/skillfrench/:uuid", async (req, res) => {
+    const query = `select * from SkillTranslation where SkillUUId = '${req.params.uuid}'`;
+    try {
+      const [rows, _] = await connection.query(query);
+      if (rows.length > 0) {
+        res.json(rows[0].French);
+      } else {
+        res.json("");
+      }
+    } catch (e) {
+      res.status(500);
+      res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
     }
-  } catch (e) {
-    res.status(500);
-    res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
-  }
-});
+  });
 
 // job autocomplete
-app.get("/autocomplete/:str", async (req, res) => {
-  const query = `select ID as jobId,French as jobTitle from Job where French like '%${req.params.str}%'`;
-  try {
-    const resp = await asyncQuery(query);
-    res.json(resp);
-  } catch (e) {
-    res.status(500);
-    res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
-  }
-});
+  app.get("/autocomplete/:str", async (req, res) => {
+    const query = `select ID as jobId,French as jobTitle from Job where French like '%${req.params.str}%'`;
+    try {
+      const [rows, _] = await connection.query(query);
+      res.json(rows);
+    } catch (e) {
+      res.status(500);
+      res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
+    }
+  });
 
 // job id to find skills
-app.get("/relatedskills/:jobid", async (req, res) => {
-  const query = `
+  app.get("/relatedskills/:jobid", async (req, res) => {
+    const query = `
   select E.ID as elementId,E.French as elementTitle from JobElements as JE
   left join Element as E on JE.ElementID = E.ID
   where JE.JobID = ${req.params.jobid} and E.ElementGroup = 'Skills' order by importanceValue desc
   `;
-  try {
-    const resp = await asyncQuery(query);
-    res.json(resp);
-  } catch (e) {
-    res.status(500);
-    res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
-  }
-});
+    try {
+      const [rows, _]  = await connection.query(query);
+      res.json(rows);
+    } catch (e) {
+      res.status(500);
+      res.send(`Something went wrong querying:<br/>${query}<br/>Error: ${e.message}`);
+    }
+  });
 
-app.listen(8080, function () {
-  console.log("server address is http://localhost:%s", 8080);
-});
+  app.listen(8080, function () {
+    console.log("server address is http://localhost:%s", 8080);
+  });
+
+})();
